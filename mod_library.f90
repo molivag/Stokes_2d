c1b2ce30373343fc5fd721176d1fcdd389c174d8
M_OlivaG <marco.oliva@upc.edu>
2021-08-02 14:09:42 +0200

    mod_library: Se quitaron las variables globales y se agrego use Parameters

--- mod_library.f90
+++ mod_library.f90
@@ -1,23 +1,24 @@
 module library
+  use Parameters
   use Isoparametric
   Implicit None
 
   ! ! ! Aqui se declaran las variables globales ! ! !
 
-  integer, parameter :: DimPr     = 2     !Dimension del problema 
-  integer, parameter :: Nelem     = 100   !Number of elements
-  integer, parameter :: n_nodes   = 341   !Total number of velocity nodes
-  integer, parameter :: n_pnodes  = 121   !Total number of preasure nodes MAXVAL(pnodes,2)
-  integer, parameter :: nUne      = 8     !Number of velocity nodes in the element
-  integer, parameter :: nPne      = 4     !Number of preasure nodes in the element
-  integer, parameter :: Dof       = 3     !Degrees of fredoom: 2 for velocity + 1 one for preasure
-  integer, parameter :: ngp       = 2     !Number of Gauss points for quadrature  
+  ! integer, parameter :: DimPr     = 2     !Dimension del problema 
+  ! integer, parameter :: Nelem     = 100   !Number of elements
+  ! integer, parameter :: n_nodes   = 341   !Total number of velocity nodes
+  ! integer, parameter :: n_pnodes  = 121   !Total number of preasure nodes MAXVAL(pnodes,2)
+  ! integer, parameter :: nUne      = 8     !Number of velocity nodes in the element
+  ! integer, parameter :: nPne      = 4     !Number of preasure nodes in the element
+  ! integer, parameter :: Dof       = 3     !Degrees of fredoom: 2 for velocity + 1 one for preasure
+  ! integer, parameter :: ngp       = 2     !Number of Gauss points for quadrature  
 
-  integer, dimension(Nelem, nUne + 1) :: elements
-  integer, dimension(Nelem, nPne + 1) :: pelements  
-  real,    dimension(n_nodes, DimPr + 1) :: nodes
-  integer, dimension(n_nodes, 2)      :: pnodes
-  real                                :: materials
+  ! integer, dimension(Nelem, nUne + 1) :: elements
+  ! integer, dimension(Nelem, nPne + 1) :: pelements  
+  ! real,    dimension(n_nodes, DimPr + 1) :: nodes
+  ! integer, dimension(n_nodes, 2)      :: pnodes
+  ! real                                :: materials
 
 
 
