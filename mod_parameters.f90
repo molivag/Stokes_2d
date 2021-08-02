88f09a0b81763694f05d83d1337c24a1267d59db
M_OlivaG <marco.oliva@upc.edu>
2021-08-02 17:15:01 +0200

    parameters: Se quito nne y se agrego len en character

--- mod_parameters.f90
+++ mod_parameters.f90
@@ -10,19 +10,31 @@ module Parameters
   integer, parameter :: Dof       = 3     !Degrees of fredoom: 2 for velocity + 1 one for preasure
   integer, parameter :: ngp       = 2     !Number of Gauss points for quadrature  
 
-  integer, dimension(Nelem, nUne + 1) :: elements
-  integer, dimension(Nelem, nPne + 1) :: pelements  
-  real,    dimension(n_nodes, DimPr + 1) :: nodes
-  integer, dimension(n_nodes, 2)      :: pnodes
-  real                                :: materials
+  integer, dimension(Nelem, nUne + 1)   :: elements
+  integer, dimension(Nelem, nPne + 1)   :: pelements  
+  real,    dimension(n_nodes, DimPr + 1):: nodes
+  integer, dimension(n_nodes, 2)        :: pnodes
+  real                                  :: materials
+
+  ! integer, dimension(100,9) :: elements
+  ! integer, dimension(100,5) :: pelements  
+  ! real,    dimension(341,3) :: nodes
+  ! integer, dimension(341,2) :: pnodes
+  ! real                      :: materials
+
+
+  ! integer, parameter        :: Nelem = size(elements,1)
+  ! integer, parameter        :: n_nodes = size(nodes,1)
+  ! integer, parameter        :: n_pnodes = 121 !Duda, como lo tomo desde el txt es decir   n_pnodes = maxval(pnodes(:,2))
+  ! integer, parameter        :: nUne = size(elements,2)-1    !Number of velocity nodes in the element
+  ! integer, parameter        :: nPne = size(pelements,2)-1   !Number of preasure nodes in the eleement
+  ! integer, parameter        :: DimPr = size(nodes,2)-1   !Dimension del problema
 
   != = = = = =  About Isoparametric Mapping  = = = = =  
-
-
   
   double precision, allocatable, dimension(:,:) :: gauss_points, gauss_weights !Verificar si debe ser global---> Si, se usa en la funcion ComputeK
-  integer       :: totGp, Nne
-  character(6)  :: ElemType !could be: Trian, Quad
+  character(len=6)  :: ElemType !could be: Trian, Quad
+  integer           :: totGp
 
   
 end module Parameters
